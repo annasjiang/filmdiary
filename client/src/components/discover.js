3d905eff249d7c204fc2257f2dddbf92aea351ca
Anna Jiang <36720819+annasjiang@users.noreply.github.com>
2023-04-10 23:19:54 -0400

    maybe runnin python?

--- client/src/components/discover.js
+++ client/src/components/discover.js
@@ -11,8 +11,6 @@ export default function Discover() {
     poster: "",
   }]);
 
-  const [recentReviews, setRecentReviews] = useState([]);
-
   useEffect(() => {
     async function fetchData() {
         const trendFetch = await axios(
@@ -62,68 +60,6 @@ export default function Discover() {
   );
   }
 
-  // recent reviews
-  const [reviews, setReviews] = useState([]);
-
-  // This method fetches the reviews from the database.
-  useEffect(() => {
-    async function getReviews() {
-      const response = await fetch(`http://localhost:4000/recentreviews/`);
-      if (!response.ok) {
-        const message = `An error occured: ${response.statusText}`;
-        window.alert(message);
-        return;
-      }
-      const reviews = await response.json();
-      setReviews(reviews);
-    }
-    getReviews();
-    return; 
-  }, [reviews.length]);
-
-
-  // This method will map out the reviews on the table
-  function reviewList() {
-    return reviews.map((review) => {
-      return (
-        <Review
-          review={review}
-          key={review._id}
-        />
-      );
-    });
-  }
-
-  // get recent reviews
-  const Review = (props) => (
-    <a href={`/review/${props.review._id}`} style={{ textDecoration: 'none', color: 'black'}}>
-    <tr> 
-      {/* use thumbnails */}
-      <td class="col-md-2">
-        {
-          props.review.poster === "http://localhost:3000/static/media/defaultposter.71253f31.jpeg" ? 
-          (<img src={defaultposter} class="img-fluid" alt="poster"/>) : 
-          (<img src={`http://image.tmdb.org/t/p/w185${props.review.poster.substring(34, 250)}`} class="img-fluid" alt="poster"/>)
-        }
-      </td>
-      <td>
-        <b style={{display: "inline", marginRight: 5}}>{props.review.name}</b>
-        <p className='text-muted' style={{fontSize: 14, display: "inline"}}> ({props.review.year})</p> 
-        <br></br>
-        <p class="text-muted">watched on {props.review.date}</p>
-        {/* <Rating
-            name="simple-controlled"
-            defaultValue={props.review.rating}
-            value={props.review.rating}
-            size="small"
-            readOnly/> <br></br> */}
-        <p>{props.review.review}</p>
-      </td>
-    </tr>
-    </a>
-  );
-
-
   return (
     <div className="table-container" style={{marginTop: 100, marginLeft: 300, marginRight: 300}}>
       <h3 style={{paddingBottom: 10}}>Discover</h3>
@@ -132,9 +68,6 @@ export default function Discover() {
         {generateTrending()}
         <h5 style={{paddingLeft: 18, paddingTop: 20}}>Recommendations</h5>
         <p>// TODO</p>
-        <table className="table table-hover" style={{ marginTop: 20 }}>
-          <tbody>{reviewList()}</tbody>
-        </table>
       </div>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+// for python
+const { spawn } = require('child_process')
+
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
@@ -7,6 +10,26 @@ app.use(cors());
 app.use(express.json());
 app.use(require("./routes/review"));
 app.use(require("./routes/list"));
+
+// for python
+app.get('/', (req, res) => {
+ 
+  var dataToSend;
+  // spawn new child process to call the python script
+  const python = spawn('python', ['script1.py']);
+  // collect data from script
+  python.stdout.on('data', function (data) {
+   console.log('Pipe data from python script ...');
+   dataToSend = data.toString();
+  });
+  // in close event we are sure that stream from child process is closed
+  python.on('close', (code) => {
+  console.log(`child process close all stdio with code ${code}`);
+  // send data to browser
+  res.send(dataToSend)
+  });
+  
+ })
 
 // get driver connection
 const dbo = require("./db/conn");
